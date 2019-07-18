0096e079848ad4cfa4b0eeba7552e88f7f091901
Robert Sirre <robert@cs.nl>
2019-07-18 15:38:14 +0200

    Migrated lib to .Net standard

---
+++ CS.Changelog/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace CS.Changelog
+{
+    /// <summary>
+    /// Reusable constant values
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// The version number of CS.Changelog
+        /// </summary>
+        /// <remarks>When changing, also change the version number in the installer.</remarks>
+        public const string Version = "1.2.0.0";
+    }
+}
