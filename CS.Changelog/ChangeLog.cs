6af8997b331877ec9ca83ebbfe410b828c51b721
Robert Sirre <robert@cs.nl>
2019-07-26 08:40:42 +0200

    [Fix] Made some changelog properties settable
    - RepositoryUrl
    - IssueNumberRegex
    - IssueTrackerUrl
    In some cases these properties may need to be adjusted. In our case a link to the issue tracker was conditional.

--- CS.Changelog/ChangeLog.cs
+++ CS.Changelog/ChangeLog.cs
@@ -19,20 +19,20 @@ namespace CS.Changelog
 		/// <seealso cref="ExportOptions.RepositoryUrl"/>
 		[JsonProperty(Order = 1)]
         [SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "<Pending>")]
-        public string RepositoryUrl { get; internal set; }
+        public string RepositoryUrl { get; set; }
 
 		/// <summary>Gets the issue number regex. Allows serialized, unformatted exports to recognize references to issues.</summary>
 		/// <value>The issue number regex.</value>
 		/// <seealso cref="ExportOptions.IssueNumberRegex"/>
 		[JsonProperty(Order = 3)]
-		public string IssueNumberRegex { get; internal set; }
+		public string IssueNumberRegex { get; set; }
 
 		/// <summary>Gets the issue tracker URL. Allows serialized, unformatted exports to create links to issue details.</summary>
 		/// <value>The issue tracker URL.</value>
 		/// <seealso cref="ExportOptions.IssueTrackerUrl"/>
 		[JsonProperty(Order = 2)]
         [SuppressMessage("Design", "CA1056:Uri properties should not be strings", Justification = "<Pending>")]
-        public string IssueTrackerUrl { get; internal set; }
+        public string IssueTrackerUrl { get; set; }
 
 		/// <summary>Gets or sets the change sets. For serialization purposes only</summary>
 		/// <value>The change sets.</value>
